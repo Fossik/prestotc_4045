7e124cafd8d54f4432d02a876282fb0487f019c6
Administrator <Administrator@DESKTOP-099UQRG>
2020-02-16 21:27:36 -0500

    created temp getter and setters

--- src/main/java/com/prestotc/dto/SpecimenDTO.java
+++ src/main/java/com/prestotc/dto/SpecimenDTO.java
@@ -2,10 +2,28 @@ package com.prestotc.dto;
 
 public class SpecimenDTO {
 	/*
-	 * This is all for example right now, so I can understand the objective, in relation to the plantplaces example.
+	 * This is all for example right now, so I can understand the objective, in relation to the plantplaces example. 
 	 */
 
 	private int specimentID;
 	private String latitude;
 	private String longitude;
+	public int getSpecimentID() {
+		return specimentID;
+	}
+	public void setSpecimentID(int specimentID) {
+		this.specimentID = specimentID;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
 }
